8b90bda0c8ef65d7ed4e682533ad648c53fdf235
Sanskar-1singh <sanskarsingh812@gmail.com>
2025-11-16 10:59:24 +0530

    Setup the booking Service

---
+++ src/main/java/com/example/uberbookingservice/Service/BookingServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.uberbookingservice.Service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookingServiceImpl implements IBookingService{
+    @Override
+    public void createBooking() {
+
+    }
+
+}
---
+++ src/main/java/com/example/uberbookingservice/Service/IBookingService.java
@@ -0,0 +1,7 @@
+package com.example.uberbookingservice.Service;
+
+public interface IBookingService  {
+
+    public void createBooking();
+
+}
